5d170484dcb3c994440c2b519a5681426a8ab9ed
Anyhow-crane <582538746@qq.com>
2019-04-11 17:45:30 +0800

    Update wise-init-docker.sh

--- wise-init-docker.sh
+++ wise-init-docker.sh
@@ -78,7 +78,8 @@ wise_command_exists() {
 wise_add_dns() {
 	dns=$(grep -c "nameserver 114.114.114.114" /etc/resolv.conf)
 	if [ "$dns" -eq '0' ]; then
-		$wise_bash_c "echo 'nameserver 114.114.114.114' >> /etc/resolv.conf"
+		$wise_bash_c "echo 'nameserver 223.5.5.5' >> /etc/resolv.conf"
+		$wise_bash_c "echo 'nameserver 223.6.6.6' >> /etc/resolv.conf"
 		echo "添加dns 114.114.114.114 "
 	fi
 }
